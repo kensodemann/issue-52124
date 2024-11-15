13ee3900cabd3757df0ca7ae755fd8917665274f
Ken Sodemann <ken.sodemann@gmail.com>
2024-11-14 20:06:13 -0600

    implement IAB

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonItem } from '@ionic/angular/standalone';
+import { DefaultSystemBrowserOptions, DefaultWebViewOptions, InAppBrowser } from '@capacitor/inappbrowser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonItem, IonList, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
+
+  openExternal() {
+    InAppBrowser.openInExternalBrowser({
+      url: 'https://www.google.com',
+    });
+  }
+
+  openSystem() {
+    InAppBrowser.openInSystemBrowser({
+      url: 'https://www.google.com',
+      options: DefaultSystemBrowserOptions,
+    });
+  }
+
+  openWebView() {
+    InAppBrowser.openInWebView({
+      url: 'https://www.google.com',
+      options: DefaultWebViewOptions,
+    });
+  }
 }
